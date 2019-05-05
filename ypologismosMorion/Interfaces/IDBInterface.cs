e669ebb6edfdb949a581bb139ad3f05d655a9f23
Chris Vamvalis <vamvalis@csd.auth.gr>
2019-05-06 00:37:18 +0300

    Added database file, connection to db

---
+++ ypologismosMorion/Interfaces/IDBInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ypologismosMorion.Interfaces
+{
+    public interface IDBInterface
+    {
+        SQLiteConnection CreateConnection();
+    }
+}
---
+++ ypologismosMorion/Models/VaseisDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ypologismosMorion.Models
+{
+    class VaseisDB
+    {
+        public string idryma { get; set; }
+        public int kodikos { get; set; }
+        public string onoma { get; set; }
+        public string eidosthesis { get; set; }
+        public int moria18 { get; set; }
+        public int moria17 { get; set; }
+        public int diafora { get; set; }
+        public double diaforatisekato { get; set; }
+    }
+}
